d5d5bfc9b3736df9699d272651fd46a5b9ce305b
Dexter <dextersimch@gmail.com>
2022-06-26 16:40:17 +0800

    item displays in inventory after pick up

--- Orbital 2D/Assets/Scripts/InventoryManager.cs
+++ Orbital 2D/Assets/Scripts/InventoryManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryManager : MonoBehaviour
 {
     public static InventoryManager Instance;
     public List<Item> items = new List<Item>();
+
+    public Transform ItemContent;
+    public GameObject InventoryItem;
 
     private void Awake()
     {
@@ -20,5 +25,22 @@ public class InventoryManager : MonoBehaviour
     public void Remove(Item item)
     {
         items.Remove(item);
+    }
+
+    public void ListItems()
+    {
+        foreach (Transform item in ItemContent)
+        {
+            Destroy(item.gameObject);
+        }
+        
+        foreach (var item in items)
+        {
+            GameObject obj = Instantiate(InventoryItem, ItemContent);
+            var itemName = obj.transform.Find("ItemName").GetComponent<TMPro.TextMeshProUGUI>();
+            var itemIcon = obj.transform.Find("ItemIcon").GetComponent<Image>();
+            itemName.text = item.itemName;
+            itemIcon.sprite = item.icon;
+        }
     }
 }
