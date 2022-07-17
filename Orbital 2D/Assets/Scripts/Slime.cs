01b2573b856caf52ff57014d2c81076d0b1bc88c
Dexter <dextersimch@gmail.com>
2022-07-17 16:29:43 +0800

    slime walking animation

--- Orbital 2D/Assets/Scripts/Slime.cs
+++ Orbital 2D/Assets/Scripts/Slime.cs
@@ -7,6 +7,7 @@ public class Slime : Enemy
     
     private Rigidbody2D myRigidbody;
     private BoxCollider2D myBoxCollider;
+    private SpriteRenderer mySpriteRenderer;
     public Transform target;
     public float chaseRadius;
     public float attackRadius;
@@ -18,6 +19,7 @@ public class Slime : Enemy
         currentState = EnemyState.idle;
         myRigidbody = GetComponent<Rigidbody2D>();
         myBoxCollider = GetComponent<BoxCollider2D>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
         anim = GetComponent<Animator>();
         target = GameObject.FindWithTag("Player").transform;
         Physics2D.IgnoreCollision(myBoxCollider, target.GetComponent<CircleCollider2D>());
@@ -26,16 +28,15 @@ public class Slime : Enemy
 	// Update is called once per frame
 	void FixedUpdate () {
         CheckDistance();
-	}
+    }
 
     void CheckDistance()
     {
-        if(Vector3.Distance(target.position, 
-                            transform.position) <= chaseRadius
-           && Vector3.Distance(target.position,
-                               transform.position) > attackRadius)
+        if(Vector3.Distance(target.position, transform.position) <= chaseRadius
+           && Vector3.Distance(target.position, transform.position) > attackRadius)
         {
-            if (currentState == EnemyState.idle || currentState == EnemyState.walk
+            if (currentState == EnemyState.idle 
+                || currentState == EnemyState.walk
                 && currentState != EnemyState.stagger)
             {
                 Vector3 temp = Vector3.MoveTowards(transform.position,
@@ -44,6 +45,7 @@ public class Slime : Enemy
                 myRigidbody.MovePosition(temp);
                 ChangeState(EnemyState.walk);
                 anim.SetBool("wakeUp", true);
+                this.SetFacingDirection();
             }
         } else if (Vector3.Distance(target.position,transform.position) > chaseRadius)
         {
@@ -52,32 +54,12 @@ public class Slime : Enemy
         }
     }
 
-    private void SetAnimFloat(Vector2 setVector){
-        anim.SetFloat("moveX", setVector.x);
-        anim.SetFloat("moveY", setVector.y);
+    private void SetFacingDirection()
+    {
+        float difference = target.position.x - transform.position.x;
+        mySpriteRenderer.flipX = difference > 0;
     }
-
-    private void changeAnim(Vector2 direction){
-        if(Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-        {
-            if(direction.x > 0){
-                SetAnimFloat(Vector2.right);
-            }else if (direction.x < 0)
-            {
-                SetAnimFloat(Vector2.left);
-            }
-        }else if(Mathf.Abs(direction.x) < Mathf.Abs(direction.y)){
-            if(direction.y > 0)
-            {
-                SetAnimFloat(Vector2.up);
-            }
-            else if (direction.y < 0)
-            {
-                SetAnimFloat(Vector2.down);
-            }
-        }
-    }
-
+    
     private void ChangeState(EnemyState newState){
         if (currentState != newState)
         {
