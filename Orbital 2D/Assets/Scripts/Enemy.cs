24ff61d8c0c470a36943b19f7d308c94a679981a
Dexter <dextersimch@gmail.com>
2022-07-24 00:28:49 +0800

    Add experience to enemy

--- Orbital 2D/Assets/Scripts/Enemy.cs
+++ Orbital 2D/Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -18,7 +19,9 @@ public class Enemy : MonoBehaviour
     public float maxHealth;
     public float health;
     public float moveSpeed;
+    public int experience;
     public int baseAttack;
+    protected GameObject player;
     protected Rigidbody2D myRigidbody;
     protected BoxCollider2D myBoxCollider;
     protected SpriteRenderer mySpriteRenderer;
@@ -33,6 +36,10 @@ public class Enemy : MonoBehaviour
         this.health -= damage;
         if (health <= 0)
         {
+            if (mySpriteRenderer.enabled)
+            {
+                this.player.GetComponent<PlayerAttribute>().AddExperience(this.experience);
+            }
             mySpriteRenderer.enabled = false;
             Invoke("Respawn", 5);
         }
@@ -57,7 +64,6 @@ public class Enemy : MonoBehaviour
             yield return new WaitForSeconds(knockTime);
             myRigidbody.velocity = Vector2.zero;
             currentState = EnemyState.idle;
-            myRigidbody.velocity = Vector2.zero;
         }
     }
 }
