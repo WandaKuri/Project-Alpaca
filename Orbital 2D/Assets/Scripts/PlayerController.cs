c3ae38ae4645f6d9d4b8f673ce4ef4cba1aecbec
ScarletBlanks <irisu.nita@gmail.com>
2022-07-28 19:56:30 +0800

    recheck bounds function added in PlayerController

--- Orbital 2D/Assets/Scripts/PlayerController.cs
+++ Orbital 2D/Assets/Scripts/PlayerController.cs
@@ -12,10 +12,10 @@ public class PlayerController : MonoBehaviour
     public string areaTransitionName;
     
     private bool canMove = true;
-    private Transform bottomLeftBox;
-    private Transform topRightBox;
-    private Vector3 bottomLeftLimit;
-    private Vector3 topRightLimit;
+    public Transform bottomLeftBox;
+    public Transform topRightBox;
+    public Vector3 bottomLeftLimit;
+    public Vector3 topRightLimit;
 
     // Start is called before the first frame update
     void Start()
@@ -33,7 +33,7 @@ public class PlayerController : MonoBehaviour
         bottomLeftBox = GameObject.FindGameObjectWithTag("bottomLeft").transform;
         topRightBox = GameObject.FindGameObjectWithTag("topRight").transform;
         bottomLeftLimit = new Vector3(bottomLeftBox.position.x, bottomLeftBox.position.y, bottomLeftBox.position.z);
-        topRightLimit = new Vector3(topRightBox.position.x, topRightBox.position.y, topRightBox.position.z) ;
+        topRightLimit = new Vector3(topRightBox.position.x, topRightBox.position.y, topRightBox.position.z);
     }
 
     // Update is called once per frame
@@ -64,6 +64,7 @@ public class PlayerController : MonoBehaviour
             playerRigid.velocity = Vector2.zero;
         }
 
+        recheck();
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
 
     }
@@ -79,5 +80,13 @@ public class PlayerController : MonoBehaviour
         yield return null;
         myAnimator.SetBool("attacking", false);
         yield return new WaitForSeconds(0.3f);
+    }
+
+    private void recheck()
+    {
+        bottomLeftBox = GameObject.FindGameObjectWithTag("bottomLeft").transform;
+        topRightBox = GameObject.FindGameObjectWithTag("topRight").transform;
+        bottomLeftLimit = new Vector3(bottomLeftBox.position.x, bottomLeftBox.position.y, bottomLeftBox.position.z);
+        topRightLimit = new Vector3(topRightBox.position.x, topRightBox.position.y, topRightBox.position.z);
     }
 }
